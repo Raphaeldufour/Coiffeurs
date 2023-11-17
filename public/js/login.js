4830c7ef397521853c3db20a18b66a78db491e7d
Dylan <la228628@student.helha.be>
2023-11-17 23:52:19 +0100

    login/logout + annuler j'avais oublié

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,34 @@
 const emailContainer = document.getElementById("login-input");
 const passwordContainer = document.getElementById("password-input");
 const loginButton = document.getElementById("login-submit");
+const cancelButton = document.getElementById("cancel-button");
 
 let loginToEnter = "";
 let passwordToEnter = "";
+
+
+
+cancelButton.addEventListener("click", () => {
+    sessionStorage.setItem('isLoggedIn', 'false');
+    event.preventDefault();
+    window.location.href = "http://localhost:3000";
+});
+
+
 
 function getUserNameAndPassword() {
     return fetch('/api/logintoenter').then(response => response.json())
 }
 
+
+
 getUserNameAndPassword().then((data) => {
     console.log(data);
     loginToEnter = data.login.email;
     passwordToEnter = data.login.mdp;
+
+
+
     loginButton.addEventListener("click", checkLoginAndPassword);
 });
 
